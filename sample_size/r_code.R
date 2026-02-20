cbe2fb0c0bf92341a1a756c901ee4822f2d4e0a7
NotLui5 <luis242617@gmail.com>
2026-02-19 22:41:53 -0500

    new_updates

--- sample_size/r_code.R
+++ sample_size/r_code.R
@@ -1,5 +1,5 @@
 # Calc sample size 
-install.packages("pmsampsize")
+# install.packages("pmsampsize")
 
 # https://cran.r-project.org/web/packages/pmvalsampsize/pmvalsampsize.pdf
 # https://cran.r-project.org/web/packages/pmsampsize/pmsampsize.pdf
@@ -7,25 +7,25 @@ library(pmsampsize)
 ## Size to train model # results: 291 -> 1004 sample size
 # parameters - 13 - 18 - 31 - 38
 # Pak, et al. c-index 0.910
-pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.91, parameters = 13)
+pmsampsize(type = "b", prevalence = 0.10, cstatistic = 0.91, parameters = 16)
 pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.91, parameters = 18)
-pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.91, parameters = 31)
+# pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.91, parameters = 31)
 pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.91, parameters = 38)
 
 # Borzooei, et al. AUC 0.99
-pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.99, parameters = 13)
+pmsampsize(type = "b", prevalence = 0.10, cstatistic = 0.99, parameters = 16)
 pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.99, parameters = 18)
-pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.99, parameters = 31)
+# pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.99, parameters = 31)
 pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.99, parameters = 38)
 
 # Aida, et al. Prevalence = 0.20 , AUC 1.00 best model ELM
-# pmsampsize(type = "b", prevalence = 0.25, cstatistic = 1.00, parameters = 13)
+# pmsampsize(type = "b", prevalence = 0.25, cstatistic = 1.00, parameters = 19)
 # pmsampsize(type = "b", prevalence = 0.25, cstatistic = 1.00, parameters = 18)
 # pmsampsize(type = "b", prevalence = 0.25, cstatistic = 1.00, parameters = 31)
 # pmsampsize(type = "b", prevalence = 0.25, cstatistic = 1.00, parameters = 38)
 
 # Lee, et al. Prevalence = 0.08 - 0.28 , AUROC 0.9622
-pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.9622, parameters = 13)
+pmsampsize(type = "b", prevalence = 0.10, cstatistic = 0.9622, parameters = 16)
 pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.9622, parameters = 18)
-pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.9622, parameters = 31)
+# pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.9622, parameters = 31)
 pmsampsize(type = "b", prevalence = 0.25, cstatistic = 0.9622, parameters = 38)
